c9b0eaaaacf13a446cdd330cc58b30fcffd0f23e
sndrschnklshk <sschinkelshoek@toku-e.com>
2022-12-13 17:52:54 +0800

    Folders moved and basic example added

---
+++ examples/basic/basic.ino
@@ -0,0 +1,24 @@
+//Arduino Example
+#include "TMP6x.h"                      // Include the header
+
+#define PIN_ADC     1                   // Random ADC pin
+
+TMP61 tmp61 = TMP61(3.3f, 12);
+
+void setup() 
+{
+   Serial.begin(115200);                // Init the serial device
+   Serial.print("Ola! Feeling cool?");  // Welcome message 
+}
+
+void loop() 
+{
+    int a = analogRead(PIN_ADC);        // Read the analog value
+    float t = tmp61.GetInterpolatedTemperature(a);  // Calculate the temperature
+    
+    Serial.print("Temperature is ");    // Begin printing output
+    Serial.print(t);                    // Print the Temperature
+    Serial.println("°C");               // Print the degrees plus a newline
+
+    delay(5000);                        // Wait 5 seconds
+}
